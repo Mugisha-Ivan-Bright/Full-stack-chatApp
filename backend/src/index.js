6b3016065b5034b1492dcb629ad528c92e47f7aa
Mugisha-Ivan -Bright <mugishaivanbright250@gmail.com>
2025-05-20 14:53:40 +0200

    Fix route parameter error

--- backend/src/index.js
+++ backend/src/index.js
@@ -35,7 +35,7 @@ const __dirname = path.dirname(__filename);
 
 
 if(process.env.NODE_ENV ==="production"){
-   app.use(express.static(path.join(__dirname, ".../frontend/chatProject/dist")));
+   app.use(express.static(path.join(__dirname, "../frontend/chatProject/dist")));
 
    app.get("*", (req , res)=>{
     res.sendFile(path.join(__dirname,"../frontend/chatProject/dist/index.html"));
